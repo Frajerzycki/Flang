c6d7ce4c6593606718a5aa647ab315545f0e43e0
frajerzycki <frajerzycki@protonmail.com>
2018-10-28 00:03:23 +0200

    Shunting yard alghorithm. Function and statements parsing will be added in future

--- Parser/Parser.hs
+++ Parser/Parser.hs
@@ -1,21 +1,35 @@
 module Parser (AST(..)) where
         import Tokenizer.Tokens
-        data AST = Function String [AST] AST Int
-                | IfStatement AST AST Int
-                | Operation AST String AST Int
-                | Operand String Int
+        import Helpers.ErrorLogging
+        data AST = Function {name :: String, args :: [AST], pos ::  Int}
+                | IfStatement {cond :: AST, body :: AST, pos :: Int}
+                | Operation {left :: AST, op :: String, right :: AST}
+                | Operand {val :: String, pos :: Int} deriving Show
 
-
+        addNode :: [AST] -> String -> [AST]
+        addNode ts op = ts ++ [Operation left op right]
+                where
+                        right   = last ts
+                        left    = last $ init ts
         parseExpression :: Tokens -> [AST] -> [String] -> AST
-        parseExpression [] ts _ = last ts
+        parseExpression [] ts [] = last ts
+        parseExpression [] ts os = parseExpression [] (addNode ts (last os))
+                (init os)
+        parseExpression ((RightBracket _ _):xs) ts [] = parseExpression xs ts []
         parseExpression (x:xs) ts os
                 | isOperand x   = parseExpression xs (ts ++ [Operand (value x)
                         (lineNumber x)]) os
-                | isFunction x  = parseFunction xs ts os 1 0
-                | isOperator x  = parseExpression xs ts ([value x] ++ os)
+                | isOperator x || isLeftBracket x =
+                        parseExpression xs ts (os ++ [value x])
+                | isRightBracket x = case o of
+                        "("     -> parseExpression xs ts os
+                        _       -> parseExpression (x:xs) (addNode ts o)
+                                (init os)
                 where
-                        isBracket  (Bracket _ _ _)              = True
-                        isBracket  _                            = False
+                        isLeftBracket  (LeftBracket _ _ _)      = True
+                        isLeftBracket  _                        = False
+                        isRightBracket  (RightBracket _ _)      = True
+                        isRightBracket  _                       = False
                         isOperator (Operator _ _ _)             = True
                         isOperator _                            = False
                         isOperand (IntegerLiteral _ _ _)        = True
@@ -24,6 +38,4 @@ module Parser (AST(..)) where
                         isOperand (StringLiteral _ _ _)         = True
                         isOperand (Identifier _ _ _)            = True
                         isOperand _                             = False
-                        isFunction (Identifier _ _ _)           = True
-                                && not (null xs) && (not . isBracket $ head xs)
-                        isFunction _                            = False
+                        o                                       = last os
