f1f41df7d89568376a0b228989fef6cd5a6def93
frajerzycki <frajerzycki@protonmail.com>
2018-10-31 18:27:23 +0100

    Parser works.

--- Parser/Parser.hs
+++ Parser/Parser.hs
@@ -1,41 +1,71 @@
-module Parser (AST(..)) where
+        module Parser.Parser (AST(..), RPN(..), parse) where
         import Tokenizer.Tokens
         import Helpers.ErrorLogging
-        data AST = Function {name :: String, args :: [AST], pos ::  Int}
+        import Parser.ParserHelpers
+        import Helpers.ErrorLogging
+        import Data.Char
+        import qualified Data.Map as Map
+        data AST = FunctionCall {name :: String, args :: [AST], pos ::  Int}
                 | IfStatement {cond :: AST, body :: AST, pos :: Int}
                 | Operation {left :: AST, op :: String, right :: AST}
-                | Operand {val :: String, pos :: Int} deriving Show
+                | Operand {val :: String, pos :: Int} deriving (Show, Eq)
 
-        addNode :: [AST] -> String -> [AST]
-        addNode ts op = ts ++ [Operation left op right]
+        data RPN = RPNRightBracket {line :: Int}
+                | RPNOperator {rpnValue :: String, line :: Int}
+                | RPNOperand {rpnValue :: String, line :: Int} deriving (Show, Eq)
+
+        isRPNRightBracket (RPNRightBracket _) = True
+        isRPNRightBracket _ = False
+
+        parse :: Tokens -> [RPN] -> [RPN] -> [RPN]
+        parse [] os oq = if [] /= os
+                then
+                        if isRPNRightBracket los
+                                then logError
+                                        "There isn't bracket to close at " p1 0
+                        else if rpnValue los == "("
+                                then logError
+                                        "Unclosed bracket at  " p1 0
+                        else oqso
+                else oqso
+
+                where los = last os
+                      oqso = oq ++ reverse os
+                      p1 = line los
+
+        parse ((Operator s p1 _):xs) [] oq = parse xs [(RPNOperator s p1)] oq
+        parse ((RightBracket p1 p2):xs) [] oq = logError
+                "There isn't bracket to close at " p1 p2
+        parse (x:xs) os oq
+                | isNumber x            = parse xs os (oq ++ [RPNOperand (value x) (lineNumber x)])
+                | isFunction x          = parse xs (os ++ [RPNOperator (value x) (lineNumber x)]) oq
+                | isOperator x          = if hlos /= '('
+                        && ((isAlpha hlos || hlos == '_')
+                        || plos < pvalX
+                        || (plos == pvalX
+                        && isLeftAssociative))
+                        then parse (x:xs) ios (oq ++ [los])
+                        else parse xs (os ++ [RPNOperator (value x) (lineNumber x)]) oq
+                | isLeftBracket x       = parse xs (os ++ [RPNOperator "(" (lineNumber x)]) oq
+                | isRightBracket x      = if hlos /= '('
+                        then parse (x:xs) ios (oq ++ [los]) else parse xs ios oq
                 where
-                        right   = last ts
-                        left    = last $ init ts
-        parseExpression :: Tokens -> [AST] -> [String] -> AST
-        parseExpression [] ts [] = last ts
-        parseExpression [] ts os = parseExpression [] (addNode ts (last os))
-                (init os)
-        parseExpression ((RightBracket _ _):xs) ts [] = parseExpression xs ts []
-        parseExpression (x:xs) ts os
-                | isOperand x   = parseExpression xs (ts ++ [Operand (value x)
-                        (lineNumber x)]) os
-                | isOperator x || isLeftBracket x =
-                        parseExpression xs ts (os ++ [value x])
-                | isRightBracket x = case o of
-                        "("     -> parseExpression xs ts os
-                        _       -> parseExpression (x:xs) (addNode ts o)
-                                (init os)
-                where
-                        isLeftBracket  (LeftBracket _ _ _)      = True
-                        isLeftBracket  _                        = False
-                        isRightBracket  (RightBracket _ _)      = True
-                        isRightBracket  _                       = False
-                        isOperator (Operator _ _ _)             = True
-                        isOperator _                            = False
-                        isOperand (IntegerLiteral _ _ _)        = True
-                        isOperand (FloatingLiteral _ _ _)       = True
-                        isOperand (CharLiteral _ _ _)           = True
-                        isOperand (StringLiteral _ _ _)         = True
-                        isOperand (Identifier _ _ _)            = True
-                        isOperand _                             = False
-                        o                                       = last os
+                los                                     = last os
+                ios                                     = init os
+                hlos                                    = head $ rpnValue los
+                valX                                    = value x
+                plos                                    = lookup (rpnValue los) precedence
+                pvalX                                   = lookup valX precedence
+                isNumber (FloatingLiteral _ _ _)        = True
+                isNumber (IntegerLiteral _ _ _)         = True
+                isNumber _                              = False
+                isFunction (Identifier _ _ _)           = True
+                isFunction _                            = False
+                isOperator (Operator _ _ _)             = True
+                isOperator _                            = False
+                isLeftBracket (LeftBracket _ _)         = True
+                isLeftBracket _                         = False
+                isRightBracket (RightBracket _ _)       = True
+                isRightBracket _                        = False
+                isLeftAssociative                       = not $ [hlos] `elem`
+                        ["=", "!"]
